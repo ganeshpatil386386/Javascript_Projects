74ef59246b61bebfe8eb4c01b7235a553d090d3f
ganeshpatil386386 <ganeshpatil386386@gmail.com>
2022-06-08 19:35:00 +0530

    finished 1st project

---
+++ Background-Changer/app.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('#bg')
+const body = document.querySelector('body')
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const label = document.querySelector('#demo')
+
+body.style.backgroundColor = 'violet'
+button.addEventListener('click', e => 
+{
+    const colorIndex= parseInt(Math.random()*colors.length)
+    body.style.backgroundColor = colors[colorIndex]
+})
+
